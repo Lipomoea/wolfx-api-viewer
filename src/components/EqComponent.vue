ff4d15ac5ba139af9c33e6cb0e79ca8536661a3a
ShuFan <768068164@qq.com>
2024-07-07 18:56:23 +0800

    /

--- src/components/EqComponent.vue
+++ src/components/EqComponent.vue
@@ -31,7 +31,7 @@ const eqlistList = ['jmaEqlist', 'cencEqlist']
   .container{
     border: black 1px solid;
     padding: 15px;
-    margin: 0px 20px 20px;
+    margin: 20px;
     border-radius: 10px;
     display: flex;
     flex-direction: column;
