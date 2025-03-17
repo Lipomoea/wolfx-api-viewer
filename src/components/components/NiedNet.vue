1ee56c1659f48704220f1c1935b7bbd8122d233d
Lipomoea <768068164@qq.com>
2025-03-17 16:43:19 +0800

    /

--- src/components/components/NiedNet.vue
+++ src/components/components/NiedNet.vue
@@ -78,7 +78,7 @@ const getData = async (url)=>{
         const res = await axios.get(url)
         return res
     }
-    catch (err){
+    catch {
         if(delay.value <= maxDelay - 100) delay.value += 100
     }
 }
