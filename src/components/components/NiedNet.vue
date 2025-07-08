1bd351cd5f68afabd6db000b015e76dd2eaefbb0
shufan <768068164@qq.com>
2025-07-08 10:24:21 +0800

    opt: isdense range.

--- src/components/components/NiedNet.vue
+++ src/components/components/NiedNet.vue
@@ -245,7 +245,7 @@ watch(()=>statusStore.map, newVal=>{
                     }
                     distances.sort((a, b) => a.distance - b.distance).splice(5)
                     adjStationIds[i] = distances.map(obj => obj.id)
-                    isDense[i] = distances.length == 5 && distances[4].distance <= 17.5
+                    isDense[i] = distances.length == 5 && distances[4].distance <= 21
                     const avgDist = distances.length <= 1 ? 0 : distances.reduce((sum, curr) => sum + curr.distance, 0) / (distances.length - 1)
                     expireSeconds[i] = Math.max(Math.round(avgDist / 3.5) + 3, 7)
                 }
