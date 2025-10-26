dcf7bbe149d6159cec40d98e92bd9bbdc4cd3ea0
Lipomoea <768068164@qq.com>
2025-10-26 17:31:48 +0800

    /

--- src/stores/status.js
+++ src/stores/status.js
@@ -678,6 +678,8 @@ export const useStatusStore = defineStore('statusStore', {
                                 eqMessage.maxIntensityText = '预估最大烈度: ' + eqMessage.maxIntensity
                                 break
                             case 1:
+                                if(calcTimeDiff(data.createTime, 8, eqMessage.reportTime, 8) < 3000)
+                                    break
                                 eqMessage.id = data.eventId
                                 eqMessage.reportTime = data.createTime
                                 const cedingIndex = data.infoTypeName.indexOf('测定')
