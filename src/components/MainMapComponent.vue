0771aa54503151912493dc6c9e40dded40af96aa
Lipomoea <768068164@qq.com>
2026-04-17 21:57:58 +0800

    fix: classical geojson loader fault style.

--- src/components/MainMapComponent.vue
+++ src/components/MainMapComponent.vue
@@ -918,8 +918,8 @@ const loadMaps = async (retries = 0) => {
         watch(()=>settingsStore.mainSettings.displayCnFault, newVal => {
             if(cnFaultBaseMap && map.hasLayer(cnFaultBaseMap)) map.removeLayer(cnFaultBaseMap)
             if(newVal) {
-                cnFaultBaseMap = loadBaseMap(cn_fault, 'faultBasePane', true, (properties) => ({
-                    color: properties.age == 'Qh' ? 'red' : properties.age == 'Qp3' ? 'orange' : 'green',
+                cnFaultBaseMap = loadBaseMap(cn_fault, 'faultBasePane', true, (item) => ({
+                    color: (item.properties ?? item).age == 'Qh' ? 'red' : (item.properties ?? item).age == 'Qp3' ? 'orange' : 'green',
                     opacity: 0.5,
                     weight: 1.5,
                 }))
